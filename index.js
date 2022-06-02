565e017be908ec285382adb620fcf66845b2789c
Clayten Zeigler <zclayten@gmail.com>
2022-06-01 20:17:23 -0400

    finished step 4

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+npc.walkEasy(500, () => {
+    npc.walkNorth()         
+})
 
 // Create the inventory
 const inventory = newInventory()
@@ -20,3 +22,5 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+
